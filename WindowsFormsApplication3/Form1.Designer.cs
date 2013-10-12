c93844b0353a94427fe0084de8fc4054f72d227d
sakchai27 <229@hotmail.com>
2013-10-12 23:36:03 +0700

    แบบสอบถาม
    แบบสอบถาม

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.bpt1 = new System.Windows.Forms.RadioButton();
+            this.bpt2 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.apt1 = new System.Windows.Forms.RadioButton();
+            this.cpt3 = new System.Windows.Forms.RadioButton();
+            this.cpt2 = new System.Windows.Forms.RadioButton();
+            this.cpt1 = new System.Windows.Forms.RadioButton();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.dpt3 = new System.Windows.Forms.RadioButton();
+            this.dpt2 = new System.Windows.Forms.RadioButton();
+            this.dpt1 = new System.Windows.Forms.RadioButton();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdCheck = new System.Windows.Forms.Button();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton3);
+            this.groupBox4.Controls.Add(this.radioButton1);
+            this.groupBox4.Controls.Add(this.bpt1);
+            this.groupBox4.Controls.Add(this.bpt2);
+            this.groupBox4.Location = new System.Drawing.Point(93, 134);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(165, 118);
+            this.groupBox4.TabIndex = 35;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "2.อายุ";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 95);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "46 ปีขึ้นไป";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 75);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 17);
+            this.radioButton1.TabIndex = 14;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "36-45 ปี";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // bpt1
+            // 
+            this.bpt1.AutoSize = true;
+            this.bpt1.Location = new System.Drawing.Point(6, 29);
+            this.bpt1.Name = "bpt1";
+            this.bpt1.Size = new System.Drawing.Size(61, 17);
+            this.bpt1.TabIndex = 12;
+            this.bpt1.Text = "18-25 ปี";
+            this.bpt1.UseVisualStyleBackColor = true;
+            // 
+            // bpt2
+            // 
+            this.bpt2.AutoSize = true;
+            this.bpt2.Location = new System.Drawing.Point(6, 52);
+            this.bpt2.Name = "bpt2";
+            this.bpt2.Size = new System.Drawing.Size(61, 17);
+            this.bpt2.TabIndex = 13;
+            this.bpt2.TabStop = true;
+            this.bpt2.Text = "26-35 ปี";
+            this.bpt2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 22);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "หญิง";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.apt1);
+            this.groupBox3.Location = new System.Drawing.Point(93, 83);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(165, 45);
+            this.groupBox3.TabIndex = 34;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "1.เพศ";
+            // 
+            // apt1
+            // 
+            this.apt1.AutoSize = true;
+            this.apt1.Location = new System.Drawing.Point(6, 21);
+            this.apt1.Name = "apt1";
+            this.apt1.Size = new System.Drawing.Size(43, 17);
+            this.apt1.TabIndex = 7;
+            this.apt1.Text = "ชาย";
+            this.apt1.UseVisualStyleBackColor = true;
+            // 
+            // cpt3
+            // 
+            this.cpt3.AutoSize = true;
+            this.cpt3.Location = new System.Drawing.Point(6, 74);
+            this.cpt3.Name = "cpt3";
+            this.cpt3.Size = new System.Drawing.Size(102, 17);
+            this.cpt3.TabIndex = 19;
+            this.cpt3.TabStop = true;
+            this.cpt3.Text = "สูงกว่าปริญญาตรี";
+            this.cpt3.UseVisualStyleBackColor = true;
+            // 
+            // cpt2
+            // 
+            this.cpt2.AutoSize = true;
+            this.cpt2.Location = new System.Drawing.Point(6, 51);
+            this.cpt2.Name = "cpt2";
+            this.cpt2.Size = new System.Drawing.Size(73, 17);
+            this.cpt2.TabIndex = 18;
+            this.cpt2.TabStop = true;
+            this.cpt2.Text = "ปริญญาตรี";
+            this.cpt2.UseVisualStyleBackColor = true;
+            // 
+            // cpt1
+            // 
+            this.cpt1.AutoSize = true;
+            this.cpt1.Location = new System.Drawing.Point(6, 28);
+            this.cpt1.Name = "cpt1";
+            this.cpt1.Size = new System.Drawing.Size(103, 17);
+            this.cpt1.TabIndex = 17;
+            this.cpt1.TabStop = true;
+            this.cpt1.Text = "ต่ำกว่าปริญญาตรี";
+            this.cpt1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.cpt3);
+            this.groupBox5.Controls.Add(this.cpt2);
+            this.groupBox5.Controls.Add(this.cpt1);
+            this.groupBox5.Location = new System.Drawing.Point(93, 258);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(159, 108);
+            this.groupBox5.TabIndex = 36;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "3.การศึกษา";
+            // 
+            // dpt3
+            // 
+            this.dpt3.AutoSize = true;
+            this.dpt3.Location = new System.Drawing.Point(6, 79);
+            this.dpt3.Name = "dpt3";
+            this.dpt3.Size = new System.Drawing.Size(91, 17);
+            this.dpt3.TabIndex = 22;
+            this.dpt3.TabStop = true;
+            this.dpt3.Text = "บุคคลภายนอก";
+            this.dpt3.UseVisualStyleBackColor = true;
+            // 
+            // dpt2
+            // 
+            this.dpt2.AutoSize = true;
+            this.dpt2.Location = new System.Drawing.Point(5, 56);
+            this.dpt2.Name = "dpt2";
+            this.dpt2.Size = new System.Drawing.Size(67, 17);
+            this.dpt2.TabIndex = 21;
+            this.dpt2.TabStop = true;
+            this.dpt2.Text = "นักศักษา";
+            this.dpt2.UseVisualStyleBackColor = true;
+            // 
+            // dpt1
+            // 
+            this.dpt1.AutoSize = true;
+            this.dpt1.Location = new System.Drawing.Point(5, 33);
+            this.dpt1.Name = "dpt1";
+            this.dpt1.Size = new System.Drawing.Size(107, 17);
+            this.dpt1.TabIndex = 20;
+            this.dpt1.TabStop = true;
+            this.dpt1.Text = "อาจารย์/เจ้าหน้าที่";
+            this.dpt1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.dpt3);
+            this.groupBox6.Controls.Add(this.dpt2);
+            this.groupBox6.Controls.Add(this.dpt1);
+            this.groupBox6.Location = new System.Drawing.Point(93, 372);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(228, 134);
+            this.groupBox6.TabIndex = 37;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "4. สถานภาพ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 13);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "แบบประเมินประสิทธิภาพการจัดทำโพลด์ของศูนย์วิจัยมหาลีย";
+            // 
+            // cmdCheck
+            // 
+            this.cmdCheck.Location = new System.Drawing.Point(296, 280);
+            this.cmdCheck.Name = "cmdCheck";
+            this.cmdCheck.Size = new System.Drawing.Size(100, 28);
+            this.cmdCheck.TabIndex = 33;
+            this.cmdCheck.Text = "เรียบร้อย";
+            this.cmdCheck.UseVisualStyleBackColor = true;
+            this.cmdCheck.Click += new System.EventHandler(this.cmdCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 561);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdCheck);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton bpt1;
+        private System.Windows.Forms.RadioButton bpt2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton apt1;
+        private System.Windows.Forms.RadioButton cpt3;
+        private System.Windows.Forms.RadioButton cpt2;
+        private System.Windows.Forms.RadioButton cpt1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RadioButton dpt3;
+        private System.Windows.Forms.RadioButton dpt2;
+        private System.Windows.Forms.RadioButton dpt1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdCheck;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCheck_Click(object sender, EventArgs e)
+        {
+            string str = "แบบประเมินประสิทธิภาพการจัดทำโพลด์ของศูนย์วิจัยมหาลีย :" + Environment.NewLine;
+
+
+            str = str + "1.เพศ ===";
+            if (apt1.Checked == true)
+            {
+                str = str + "ชาย" + Environment.NewLine;
+           
+
+            }
+            if (radioButton2.Checked == true)
+            {
+                str = str + "หญิง" + Environment.NewLine;
+           
+            }
+
+            str = str + "2.อายุ ===";
+            if (bpt1.Checked == true)
+            {
+                str = str + "18-25 ปี" + Environment.NewLine;
+            }
+            if (bpt2.Checked == true)
+            {
+                str = str + "26-35 ปี" + Environment.NewLine;
+            }
+            if (radioButton1.Checked == true)
+            {
+                str = str + "36-45 ปี" + Environment.NewLine;
+
+            }
+            if (radioButton3.Checked == true)
+            {
+                str = str + "46 ปีขึ้นไป" + Environment.NewLine;
+
+            }
+            str = str + "3.การศึกษา ===";
+            if (cpt1.Checked == true)
+            {
+                str = str + "ต่ำกว่าปริญญาตรี" + Environment.NewLine;
+            }
+            if (cpt2.Checked == true)
+            {
+                str = str + "ปริญญาตรี" + Environment.NewLine;
+            }
+            if (cpt3.Checked == true)
+            {
+                str = str + "สูงกว่าปริญญาตรี" + Environment.NewLine;
+
+            }
+            str = str + "4.สถานภาพ ===";
+            if (dpt1.Checked == true)
+            {
+                str = str + "อาจารย์/เจ้าหน้าที่" + Environment.NewLine;
+            }
+            if (dpt2.Checked == true)
+            {
+                str = str + "นักศักษา" + Environment.NewLine;
+            }
+            if (dpt3.Checked == true)
+            {
+                str = str + "บุคคลภายนอก" + Environment.NewLine;
+
+
+
+
+
+            }
+            MessageBox.Show(str, "รายงานผล");
+        }
+
+        private void optMale_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void apt2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
